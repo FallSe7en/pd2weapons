76ed70ae149bce5701cd19d2a804235b2344fcad
Justin Lei <lei.justin@gmail.com>
2014-04-30 21:03:35 -0700

    UAR data entered

--- js/data.js
+++ js/data.js
@@ -1988,5 +1988,535 @@ window.WeaponsData = [
                 imageUrl    : "img/mod/magazine/War_Torn_Stock.png"
             }
         ]
+    },
+    {
+        name          : "UAR",
+
+        price         : 176000,
+        magazine      : 30,
+        totalAmmo     : 120,
+        rateOfFire    : 750,
+        damage        : 28,
+        accuracy      : 14,
+        stability     : 12,
+        concealment   : 20,
+        threat        : 14,
+
+        imageUrl      : "img/gun/assault_rifle/UAR.png",
+
+        modifications : [
+            {
+                name        : "Short Barrel",
+                slot        : "Barrel",
+
+                price       : 178000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : -2,
+                stability   : 5,
+                concealment : 2,
+                threat      : 0,
+
+                imageUrl    : "img/mod/barrel/Short_Barrel_(UAR).png"
+            },
+            {
+                name        : "Long Barrel",
+                slot        : "Barrel",
+
+                price       : 263000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 2,
+                accuracy    : 2,
+                stability   : 3,
+                concealment : -3,
+                threat      : 0,
+
+                imageUrl    : "img/mod/barrel/Long_Barrel_(UAR).png"
+            },
+            {
+                name        : "Low Profile Suppressor",
+                slot        : "Barrel Extension",
+
+                price       : 102000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : -12,
+                accuracy    : 0,
+                stability   : 0,
+                concealment : 0,
+                threat      : -14,
+
+                imageUrl    : "img/mod/barrel_extension/Low_Profile_Suppressor.png"
+            },
+            {
+                name        : "Medium Suppressor",
+                slot        : "Barrel Extension",
+
+                price       : 69000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : -8,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -2,
+                threat      : -14,
+
+                imageUrl    : "img/mod/barrel_extension/Medium_Suppressor.png"
+            },
+            {
+                name        : "The Bigger The Better Suppressor",
+                slot        : "Barrel Extension",
+
+                price       : 178000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : -3,
+                accuracy    : 0,
+                stability   : 7,
+                concealment : -3,
+                threat      : -14,
+
+                imageUrl    : "img/mod/barrel_extension/The_Bigger_The_Better.png"
+            },
+            {
+                name        : "Stubby Compensator",
+                slot        : "Barrel Extension",
+
+                price       : 102000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 2,
+                accuracy    : 0,
+                stability   : 7,
+                concealment : 0,
+                threat      : 6,
+
+                imageUrl    : "img/mod/barrel_extension/Stubby.png"
+            },
+            {
+                name        : "The Tank Compensator",
+                slot        : "Barrel Extension",
+
+                price       : 139000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 5,
+                accuracy    : 0,
+                stability   : 5,
+                concealment : -1,
+                threat      : 8,
+
+                imageUrl    : "img/mod/barrel_extension/The_Tank.png"
+            },
+            {
+                name        : "Fire Breather Nozzle",
+                slot        : "Barrel Extension",
+
+                price       : 178000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 7,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -2,
+                threat      : 14,
+
+                imageUrl    : "img/mod/barrel_extension/Fire_Breather.png"
+            },
+            {
+                name        : "Tactical Compensator",
+                slot        : "Barrel Extension",
+
+                price       : 0,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 2,
+                accuracy    : 4,
+                stability   : -4,
+                concealment : -2,
+                threat      : 0,
+
+                imageUrl    : "img/mod/barrel_extension/Tactical_Compensator.png"
+            },
+            {
+                name        : "Competitor's Compensator",
+                slot        : "Barrel Extension",
+
+                price       : 0,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 5,
+                accuracy    : 2,
+                stability   : 7,
+                concealment : -2,
+                threat      : 6,
+
+                imageUrl    : "img/mod/barrel_extension/Competitors_Compensator.png"
+            },
+            {
+                name        : "Funnel of Fun Nozzle",
+                slot        : "Barrel Extension",
+
+                price       : 0,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 10,
+                accuracy    : -4,
+                stability   : 0,
+                concealment : -2,
+                threat      : 14,
+
+                imageUrl    : "img/mod/barrel_extension/Funnel_of_Fun.png"
+            },
+            {
+                name        : "Single Fire",
+                slot        : "Custom",
+
+                price       : 249200,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 2,
+                stability   : -6,
+                concealment : 0,
+                threat      : 0,
+
+                imageUrl    : "img/mod/custom/Single_Fire.png"
+            },
+            {
+                name        : "Auto Fire",
+                slot        : "Custom",
+
+                price       : 249800,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 2,
+                accuracy    : -2,
+                stability   : 5,
+                concealment : 0,
+                threat      : 0,
+
+                imageUrl    : "img/mod/custom/Auto_Fire.png"
+            },
+            {
+                name        : "Auto Fire",
+                slot        : "Custom",
+
+                price       : 249800,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 2,
+                accuracy    : -2,
+                stability   : 5,
+                concealment : 0,
+                threat      : 0,
+
+                imageUrl    : "img/mod/custom/Auto_Fire.png"
+            },
+            {
+                name        : "A3 Tactical Foregrip",
+                slot        : "Foregrip",
+
+                price       : 263000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 2,
+                accuracy    : 0,
+                stability   : 5,
+                concealment : -2,
+                threat      : 0,
+
+                imageUrl    : "img/mod/foregrip/A3_Tactical.png"
+            },
+            {
+                name        : "Assault Light",
+                slot        : "Gadget",
+
+                price       : 102000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 0,
+                concealment : -1,
+                threat      : 0,
+
+                imageUrl    : "img/mod/gadget/Assault_Light.png"
+            },
+            {
+                name        : "Tactical Laser Module",
+                slot        : "Gadget",
+
+                price       : 178000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 0,
+                concealment : -1,
+                threat      : 0,
+
+                imageUrl    : "img/mod/gadget/Tactical_Laser_Module.png"
+            },
+            {
+                name        : "Compact Laser Module",
+                slot        : "Gadget",
+
+                price       : 0,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 0,
+                concealment : 0,
+                threat      : 0,
+
+                imageUrl    : "img/mod/gadget/Compact_Laser_Module.png"
+            },
+            {
+                name        : "Military Laser Module",
+                slot        : "Gadget",
+
+                price       : 0,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -2,
+                threat      : 0,
+
+                imageUrl    : "img/mod/gadget/Military_Laser_Module.png"
+            },
+            {
+                name        : "The Professional's Choice Sight",
+                slot        : "Sight",
+
+                price       : 102000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -1,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/The_Professionals_Choice.png"
+            },
+            {
+                name        : "Surgeon Sight",
+                slot        : "Sight",
+
+                price       : 178000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -1,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Surgeon_Sight.png"
+            },
+            {
+                name        : "See More Sight",
+                slot        : "Sight",
+
+                price       : 213600,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -1,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/See_More_Sight.png"
+            },
+            {
+                name        : "Combat Sight",
+                slot        : "Sight",
+
+                price       : 0,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -1,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Combat_Sight.png"
+            },
+            {
+                name        : "Speculator Sight",
+                slot        : "Sight",
+
+                price       : 0,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -1,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Speculator_Sight.png"
+            },
+            {
+                name        : "Trigonom Sight",
+                slot        : "Sight",
+
+                price       : 0,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -1,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Trigonom_Sight.png"
+            },
+            {
+                name        : "Holographic Sight",
+                slot        : "Sight",
+
+                price       : 102000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -2,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Holographic_Sight.png"
+            },
+            {
+                name        : "Compact Holosight",
+                slot        : "Sight",
+
+                price       : 0,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -2,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Compact_Holosight.png"
+            },
+            {
+                name        : "Solar Sight",
+                slot        : "Sight",
+
+                price       : 0,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -2,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Solar_Sight.png"
+            },
+            {
+                name        : "Military Red Dot Sight",
+                slot        : "Sight",
+
+                price       : 43000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -3,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Military_Red_Dot_(Lootbag_DLC).png"
+            },
+            {
+                name        : "Military Red Dot Sight",
+                slot        : "Sight",
+
+                price       : 307000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -3,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Military_Red_Dot.png"
+            },
+            {
+                name        : "Milspec Scope",
+                slot        : "Sight",
+
+                price       : 307000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -3,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Milspec_Scope.png"
+            },
+            {
+                name        : "Acough Optic Scope",
+                slot        : "Sight",
+
+                price       : 220000,
+                magazine    : 0,
+                totalAmmo   : 0,
+                rateOfFire  : 0,
+                damage      : 0,
+                accuracy    : 0,
+                stability   : 3,
+                concealment : -3,
+                threat      : 0,
+
+                imageUrl    : "img/mod/magazine/Acough_Optic.png"
+            },
+        ]
     }
 ];
