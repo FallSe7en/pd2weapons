98e84c888401d143d0f9a69b1ef59cdb2c10428f
Justin Lei <lei.justin@gmail.com>
2014-06-28 02:26:33 -0700

    Added AK.762

--- js/data.js
+++ js/data.js
@@ -2,12 +2,14 @@ define([
     "data/amcar",
     "data/ak",
     "data/car4",
-    "data/uar"
-], function (amcar, ak, car4, uar) {
+    "data/uar",
+    "data/ak762"
+], function (amcar, ak, car4, uar, ak762) {
     return [
         amcar,
         ak,
         car4,
-        uar
+        uar,
+        ak762
     ];
 });
