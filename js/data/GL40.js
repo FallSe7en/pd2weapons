caee03a0f8e1605beb3dab6a5e6f4e732ce56b49
Justin Lei <lei.justin@gmail.com>
2014-09-09 21:06:36 -0700

    New weapon data files

---
+++ js/data/GL40.js
@@ -0,0 +1,3 @@
+define([], function () {
+    return {"name":"GL40","attributes":{"magazine":"6","damage":"1","concealment":"6","totalAmmo":"","threat":"18","rateOfFire":"37","stability":"12","accuracy":"340"},"modifications":[{"magazine":"","concealment":"+4","damage":"","totalAmmo":"","threat":"","name":"Pirate Barrel","rateOfFire":"","stability":"","slot":"Barrel","accuracy":"-4"},{"magazine":"","concealment":"+4","damage":"","totalAmmo":"","threat":"","name":"Sawed-off Stock","rateOfFire":"","stability":"-3","slot":"Stock","accuracy":""}]};
+});
