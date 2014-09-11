443744f63dbd96e13ce25c6e58d410a08c89d40b
Justin Lei <lei.justin@gmail.com>
2014-09-10 23:10:24 -0700

    Clean out data directory

--- js/data/GL40.js
+++
@@ -1,3 +0,0 @@
-define([], function () {
-    return {"name":"GL40","attributes":{"magazine":"6","damage":"1","concealment":"6","totalAmmo":"","threat":"18","rateOfFire":"37","stability":"12","accuracy":"340"},"modifications":[{"name":"Pirate Barrel","attributes":{"magazine":"","concealment":"4","damage":"","totalAmmo":"","threat":"","rateOfFire":"","stability":"","accuracy":"-4"},"slot":"Barrel"},{"name":"Sawed-off Stock","attributes":{"magazine":"","concealment":"4","damage":"","totalAmmo":"","threat":"","rateOfFire":"","stability":"-3","accuracy":""},"slot":"Stock"}]};
-});
