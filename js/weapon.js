f1a51546c99732e9db9c34a6b87e0d6c9554efbf
Justin Lei <lei.justin@gmail.com>
2014-09-17 19:28:06 -0700

    Script to translate attributes to use attribute value tables

--- js/weapon.js
+++ js/weapon.js
@@ -7,6 +7,20 @@ define([ "base", "mod" ], function (Base, Mod) {
         concealment : 30
     };
 
+    var attributeValueTables = {
+        damage: [
+            10, 11,   12, 13,   14, 15,   16,  17.5, 20,  22.5, 25, 27.5,
+            30, 32.5, 35, 37.5, 40, 42.5, 45,  47.5, 50,  55,   60, 65,
+            70, 75,   80, 85,   90, 95,   100, 105,  110, 115,  120
+        ],
+        accuracy: [ 0, 2, 4, 6, 8,    10, 12,   14, 16, 18 ],
+        stability: [ 0, 3, 6, 8, 12.5, 15, 17.5, 19, 20, 20, 21, 22, 23, 24, 25 ],
+        threat: [
+            4.5, 3.9, 3.6, 3.3, 3,   2.8, 2.6, 2.4, 2.2, 1.6,
+            1.5, 1.4, 1.3, 1.2, 1.1, 1,   0.8, 0.6, 0.4, 0.2
+        ],
+    };
+
     var Weapon = function Weapon(details) {
         var self = this;
 
@@ -147,7 +161,8 @@ define([ "base", "mod" ], function (Base, Mod) {
         });
 
         return self.attributes.map(function (attribute) {
-            var attributeValue = parseInt(self[attribute] || 0) + parseInt(stats[attribute]);
+            var attributeValue = parseInt(self[attribute] || 0) + parseInt(stats[attribute]),
+                table = attributeValueTables[attribute];
 
             if (
                 (attribute in attributeMaxes)
@@ -158,7 +173,7 @@ define([ "base", "mod" ], function (Base, Mod) {
 
             return {
                 name  : attribute,
-                value : attributeValue
+                value : table ? table[attributeValue] : attributeValue
             };
         });
     };
